8f7fe8a7363952c5b0b44afce04631922208c7e5
Murat Alkaptan <muratalkaptan1@gmail.com>
2025-03-07 15:21:07 +0300

    Help Center Fixed

--- src/HelpCenterPage.jsx
+++ src/HelpCenterPage.jsx
@@ -2,15 +2,13 @@ import Navbar from './Navbar.jsx';
 import Footer from './Footer.jsx';
 import React from 'react';
 import HelpCenter from './HelpCenter.jsx';
-import { Scroll } from 'lucide-react';
 
 export default function HelpCenterPage() {
   return (
     <div>
-        <Scroll/>
-        <Navbar />
+        <Navbar/>
         <HelpCenter />
-        <Footer />
+        <Footer/>
     </div>
   );
 }
